1c2c11da05f31d319990d4d1b7cef3b6376244d9
Shield <164216548@qq.com>
2018-09-12 02:48:03 +0000

    Finish static pages

---
+++ resources/views/static_pages/about.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts/default')
+@section('head', '关于我们')
+@section('content')
+    <h1>关于我们</h1>
+@stop
---
+++ resources/views/static_pages/help.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts/default')
+@section('head', '帮助')
+@section('content')
+    <h1>帮助</h1>
+@stop
